a315079a105d050618300f788e8fc42e289f72a5
Teemu Kallio <teemu.kallio@reaktor.com>
2024-08-23 13:45:01 +0300

    Add scales

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,12 @@
 
 MCP492X dac(SPI_SS_PIN);
 
+uint8_t prevMod2;
+uint8_t currentMod2;
+
+uint16_t *selectedScale = scale_chromatic;
+uint8_t scaleLength = sizeof(scale_chromatic) / sizeof(uint16_t);
+
 void setup() {
   Serial.begin(9600);
   Serial.println("setup");
@@ -28,16 +34,56 @@ void setup() {
   dac.begin();
 }
 
+void setScale(uint8_t scaleSelector) {
+  switch (scaleSelector) {
+    case 0:
+      selectedScale = scale_chromatic;
+      scaleLength = sizeof(scale_chromatic) / sizeof(uint16_t);
+      break;
+    case 1:
+      selectedScale = scale_major;
+      scaleLength = sizeof(scale_major) / sizeof(uint16_t);
+      break;
+    case 2:
+      selectedScale = scale_minor;
+      scaleLength = sizeof(scale_minor) / sizeof(uint16_t);
+      break;
+    case 3:
+      selectedScale = scale_major_triad;
+      scaleLength = sizeof(scale_major_triad) / sizeof(uint16_t);
+      break;
+    case 4:
+      selectedScale = scale_minor_triad;
+      scaleLength = sizeof(scale_minor_triad) / sizeof(uint16_t);
+      break;
+    case 5:
+      selectedScale = scale_pentatonic_major;
+      scaleLength = sizeof(scale_pentatonic_major) / sizeof(uint16_t);
+      break;
+    case 6:
+      selectedScale = scale_pentatonic_minor;
+      scaleLength = sizeof(scale_pentatonic_minor) / sizeof(uint16_t);
+      break;
+  }
+}
+
 void loop() {
+  currentMod2 = map(analogRead(MOD2_PIN), 0, 1023, 0, NMR_SCALES);
+  if (currentMod2 != prevMod2) {
+    setScale(currentMod2);
+  }
+
   uint16_t cvInVal = map(analogRead(MOD1_PIN), 0, 1023, 0, 4095); // input CV is 10bits (0-1023), map it to 12bits (0-4095)
 
-  for (int i = 0; i < NMR_SEMINOTES; i++) {
-    uint16_t semitone = pgm_read_word(&semitones[i]);
-    uint16_t nextSemitone = pgm_read_word(&semitones[i + 1]);
+  for (int i = 0; i < scaleLength; i++) {
+    uint16_t semitone = pgm_read_word(&selectedScale[i]);
+    uint16_t nextSemitone = pgm_read_word(&selectedScale[i + 1]);
 
     if (cvInVal >= semitone && cvInVal <= nextSemitone) {
       dac.analogWrite(semitone);
       break;
     }
   }
+
+  prevMod2 = currentMod2;
 }
